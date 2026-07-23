0b9595e707bb56a1a92f53fc63bff4b396f5d0b5
Martin Cyster <martin.cyster@unimelb.edu.au>
2026-07-24 09:53:34 +1000

    Minor cleanups

--- geoexposure/core/__init__.py
+++ geoexposure/core/__init__.py
@@ -1,9 +1,9 @@
 """Core utilities, environment modelling, and shared infrastructure."""
 
-from . import datetime_utils as datetime_utils
-from . import spatial_utils as spatial_utils
 from .cachable import Cachable
 from .enums import SamplingMethod
+from . import datetime_utils as datetime_utils
+from . import spatial_utils as spatial_utils
 
 __all__ = [
     "Cachable",
